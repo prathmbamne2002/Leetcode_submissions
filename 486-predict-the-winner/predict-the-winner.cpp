6d01347ddb3f82ce2f345a22e387b9e9f459bcf4
prathmbamne2002 <prathmbamne2002@gmail.com>
2026-07-27 14:56:58 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (69.87%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,18 +1,20 @@
 
 class Solution {
 public:
+    int dp[21][21];
     int recurr(int start,int end,vector<int>&nums){
 
         if(start>end) return 0;
-
+        if(dp[start][end]!=-1) return dp[start][end];
         int takefirst = nums[start] - recurr(start+1,end,nums);
         int takelast  = nums[end] - recurr(start,end-1,nums);
 
-        return max(takefirst,takelast);
+        return dp[start][end]=max(takefirst,takelast);
         
     }
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
+        memset(dp,-1,sizeof(dp));
         int score = recurr(0,n-1,nums);
         if(score>=0) return true;
         else return false;
