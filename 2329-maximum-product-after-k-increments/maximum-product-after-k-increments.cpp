441fff5d52b81c354c7e85a4d2b0dcf0863801a6
prathmbamne2002 <prathmbamne2002@gmail.com>
2026-09-07 18:25:56 +0530

    Time: 26 ms (99.80%) | Memory: 86 MB (99.61%) - LeetSync

--- 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.cpp
+++ 2329-maximum-product-after-k-increments/maximum-product-after-k-increments.cpp
@@ -1,27 +1,52 @@
 class Solution {
 public:
+    long long  check(int mid,vector<int>&nums,int k){
+        long long cnt = 0;
+        for(int i:nums){
+            if(i<mid) cnt+= (mid-i);
+        }
+
+        return cnt;
+    }
+
     int maximumProduct(vector<int>& nums, int k) {
         int n = nums.size();
         int mod = 1e9+7;
+        int kRem = 0;
+        
+        int lo = *min_element(nums.begin(),nums.end());
+        int hi = 1e9;
+        int ans = lo;
 
-        priority_queue<int,vector<int>,greater<int>>pq;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
 
-        for(int i:nums) pq.push(i);
+            long long kUsed = check(mid,nums,k);
 
-        while(k--){
-            int top = pq.top();
-            pq.pop();
-            pq.push(top+1);
+            if(kUsed<=k){
+                ans =  mid;
+                kRem = k-kUsed;
+                lo = mid+1;
+            }
+            else{
+                hi = mid-1;
+            }
         }
 
-        long long product = 1;
-        while(!pq.empty()){
-            int top = pq.top();
-            pq.pop();
-            product = (1ll * product * top)%mod;
-        }
+        //cout<<ans<<endl;
 
+        long long product= 1;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]<=ans && kRem) {
+                product = (1ll*product*(ans+1))%mod;
+                kRem--;
+                }
+            else if(nums[i]<ans) product = (1ll*product*ans)%mod;
+            else product = (1ll*product*nums[i])%mod;
+        }
 
         return product;
+
     }
 };
